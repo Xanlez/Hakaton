5be6fd22f39b325c910be9ce37b633dea1e6fd39
EbAs <homutov.atem@gmail.com>
2026-05-15 18:55:34 +0300

    core

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+import requests
+
+url = "https://ngw.devices.sberbank.ru:9443/api/v2/oauth"
+
+payload = {}
+headers = {
+  'Content-Type': 'application/x-www-form-urlencoded',
+  'Accept': 'application/json'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
 """Загрузка афиши с API, SQLite и вывод в консоль."""
 
 import argparse
